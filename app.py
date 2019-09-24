105b5084c4a7b1a8bb23d6bfd74f26b2b5083361
Keith Lashley <lashleykeith@gmail.com>
2019-09-24 16:44:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,61 +1,51 @@
+import csv
 import numpy as np
-from lightfm.datasets import fetch_movielens
-from lightfm import LightFM
+from sklearn.svm import SVR
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.get_backend()
 
-# CHALLENGE part 1 of 3 - write your own fetch and format method for a different recommendation
-# dataset. Here a good few https://gist.github.com/entaroadun/1653794
-# And take a look at the fetch_movielens method to see what it's doing
-#
-
-
-#fetch data and format it and rating minimum
-data = fetch_movielens(min_rating=4.0)
-
-# print training and testing data
-print(repr(data['train']))
-print(repr(data['test']))
-
-# CHALLENGE part 2 of 3 - use 3 different loss functions (so 3 different models), compare results, print results for
-# the best one. - Available loss functions are warp, logistic, bpr, and warp-kos.
-
-#create model Weight Approximate-Rank Pairwise
-# Gradient Descent Algorthim
-# Hybrid system using both content and collaborative
-model = LightFM(loss='warp')
-# train model
-model.fit(data['train'], epochs=30, num_threads=2)
+dates = []
+prices = []
 
 
-# CHALLENGE part 3 of 3 - Modify this function so that it parses your dataset correctly to retrieve
-# the necessary variables (products, songs, tv shows, etc.)
-# then print out the recommended results
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[1]))
+    return
 
-def sample_recommendation(model, data, user_ids):
-    # number of users and movies in training data
-    n_users, n_items = data['train'].shape
 
-    # generate recommendations for each user we input
-    for user_id in user_ids:
+def predict_price(dates, prices, x):
+    dates = np.reshape(dates, (len(dates), 1))  # converting to matrix of n X 1
 
-        # movies they already like
-        known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
+    svr_lin = SVR(kernel='linear', C=1e3)
+    svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)  # defining the support vector regression models
+    svr_rbf.fit(dates, prices)  # fitting the data points in the models
+    svr_lin.fit(dates, prices)
+    svr_poly.fit(dates, prices)
 
-        # movies our model predicts they will like
-        scores = model.predict(user_id, np.arange(n_items))
-        # rank them in order of most liked to least
-        top_items = data['item_labels'][np.argsort(-scores)]
+    plt.scatter(dates, prices, color='black', label='Data')  # plotting the initial datapoints
+    plt.plot(dates, svr_rbf.predict(dates), color='red', label='RBF model')  # plotting the line made by the RBF kernel
+    plt.plot(dates, svr_lin.predict(dates), color='green',
+             label='Linear model')  # plotting the line made by linear kernel
+    plt.plot(dates, svr_poly.predict(dates), color='blue',
+             label='Polynomial model')  # plotting the line made by polynomial kernel
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Support Vector Regression')
+    plt.legend()
+    plt.show()
 
-        # print out the results
-        print("User %s" % user_id)
-        print("     Known positives:")
+    return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
-        for x in known_positives[:3]:
-            print("        %s" % x)
 
-        print("     Recommended:")
+get_data('GOOGL.csv')  # calling get_data method by passing the csv file to it
+# print "Dates- ", dates
+# print "Prices- ", prices
 
-        for x in top_items[:3]:
-            print("        %s" % x)
-
-# three random user ids
-sample_recommendation(model, data, [3, 25, 450])
+predicted_price = predict_price(dates, prices, 29)
